c56cac85dbf4aec2af8f922ca8980c9472a41921
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2021-10-16 15:22:43 +0200

    update config

--- config/mes_options.php
+++ config/mes_options.php
@@ -2,5 +2,18 @@
 
     // define('_NO_CACHE', -1);
 
+    // $GLOBALS['oembed_providers'] = array(
+	//        'https://videos.domainepublic.net/videos/watch/*' => 'https://videos.domainepublic.net/services/oembed',
+    // );
+
+    $GLOBALS['spip_pipeline']['newsletter_pre_envoi'] .= "|newsletter_dkim";
+    function newsletter_dkim($mailer){
+        $mailer->DKIM_domain = 'ammd.net';
+        $mailer->DKIM_selector = 'ammd';
+        $mailer->DKIM_private = dirname(__FILE__).'/dkim.key';
+        $mailer->DKIM_passphrase = '';
+        $mailer->DKIM_identity = $mail->From;
+        return $mailer;
+    }
 
 ?>
